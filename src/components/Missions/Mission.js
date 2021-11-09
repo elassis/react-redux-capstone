66c2ed8dcd8d1fcf748b48a400870c8b37f26687
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-09 16:12:27 -0400

    Finish adding the functionality of a button to change the state of the mission

--- src/components/Missions/Mission.js
+++ src/components/Missions/Mission.js
@@ -1,26 +1,23 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import { setReserved, setUnReserved } from '../../redux/missions/missions';
 import classes from './Missions.module.css';
 
 const Mission = ({ mission }) => {
-  const { id, name, description } = mission;
-  const joinMission = useSelector((state) => state);
+  const { name, description } = mission;
+  const [joinMission, setJoinMission] = useState(false);
   const [buttonContent, setButtonContent] = useState('Join Mission');
   const [status, setStatus] = useState('NOT A MEMBER');
-  const dispatch = useDispatch();
 
   const handleJoinMission = () => {
     if (!joinMission) {
       setButtonContent('Leave Mission');
       setStatus('Active Member');
-      dispatch(setReserved(id));
     } else if (joinMission) {
       setButtonContent('Join Mission');
       setStatus('NOT A MEMBER');
-      dispatch(setUnReserved(id));
     }
+
+    setJoinMission(() => !joinMission);
   };
 
   return (
